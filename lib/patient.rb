31cb428631b96800407b736753a52bccc3eff36b
Jesse Murphy <jessemurphy1982@gmail.com>
2020-08-12 03:16:31 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -13,8 +13,8 @@ class Patient
   end 
   
   
-   def new_appointment(date, patient)
-    Appointment.new(date, patient, self)
+   def new_appointment(date, doctor)
+    Appointment.new(date, doctor, self)
   end 
     
   
